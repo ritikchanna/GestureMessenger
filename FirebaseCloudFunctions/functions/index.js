e935508bf57311c80bb41f234fb43494d00704d7
Ritik Channa <chnritik@gmail.com>
2018-08-15 19:12:21 +0530

    Improved Firebase Function from stackoverflow

--- FirebaseCloudFunctions/functions/index.js
+++ FirebaseCloudFunctions/functions/index.js
@@ -1,61 +1,60 @@
-'use strict';
-
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 admin.firestore().settings({
-    timestampsInSnapshots: true
+  timestampsInSnapshots: true
 });
-var token = 'token_value';
-var sender = 'sender_value';
-var reciever = 'reciever_value';
-var message = 'message_value';
-var payload ='payload_value';
 var db = admin.firestore();
 
- 
+function debugLogger(msg) {
+  // comment out to disable logging
+  console.log(msg);
+}
 
+function errLogger(err) {
+  console.log(err);
+}
 
-exports.sendFollowerNotification = functions.database.ref('/m/{messageid}')
+exports.sendFollowerNotification =
+  functions.database.ref('/m/{messageid}')
     .onCreate((snapshot, context) => {
-        console.log('v21');
-         message = context.params.messageid;
-        console.log('Message Id:', message);
+      debugLogger('v23_stackoverflow');
+      const message = context.params.messageid;
+      debugLogger(`Message Id: ${message}`);
 
-         reciever = snapshot.val().r;
-        console.log('Message reciever: ', reciever);
-         sender = snapshot.val().s;
-        console.log('Message sender: ', sender);
-                    payload = {
-                        data: {
-                            id: `${message}`,
-                            sender: `${sender}`
-                        }
-                         
-               
-                    };
-        console.log('Payload Created');
+      const reciever = snapshot.val().r;
+      debugLogger(`Message reciever: ${reciever}`);
+      const sender = snapshot.val().s;
+      debugLogger(`Message sender: ${sender}`);
+      const payload = {
+        data: {
+          id: `${message}`,
+          sender: `${sender}`
+        }
+      };
+      debugLogger('Payload Created');
 
+      let tokenRef = db.collection(reciever).doc('t');
+      debugLogger('Fetching Token');
+      return tokenRef.get()
+        .then(doc => {
+          debugLogger('Fetching Token started');
+          if (!doc.exists)
+            throw new Error("Token doesnt exist");
 
-        
-        var tokenRef = db.collection(reciever).doc('t');
-        console.log('Fetching Token');
-        tokenRef.get()
-            .then(doc => {
-                console.log('Fetching Token started'); 
-                if (!doc.exists) {
-                    console.log('Token doesnt exist ');
-                } else {
-                    token = doc.data().v;
-                    console.log('Token data:', token);
-
-                }
-                
-                return admin.messaging().sendToDevice(token,payload);
-
-            })
-            .catch(err => {
-                console.log('Error sending fcm', err);
-            });
-
+          let token = doc.data().v;
+          debugLogger(`Token data: ${token}`);
+          return token;
+        })
+        .then(token => {
+          debugLogger('Sending FCM now');
+          return admin.messaging().sendToDevice(token, payload);
+        })
+        .then(() => {
+          debugLogger('Successfully sent message!');
+          return null;
+        })
+        .catch(err => {
+          errLogger(err);
+        })
     })
